95b8b7b9ce5d673c82d2f2c3e5858a2bc7b4ae58
DiamkUZ <diamkms@yandex.ru>
2023-03-16 08:00:38 +0300

    all done, but hourglass...

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        int center = sideLength / 2 + 1;
+
+        for (int i = 1; i<=sideLength; i++) {
+
+            for (int j = 1; j<sideLength; j++) {
+                if (j == center || i == center) {
+                    System.out.print("8");
+                }
+                else System.out.print(" ");
+
+            }
+            if (i == center) {
+                System.out.println("8");
+            } else System.out.println(" ");
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,32 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int evenHeight = 1;
+        if (height % 2 == 0) {
+            evenHeight = 0;
+        }
+
+        for(int i=0; i < height/2 +evenHeight; i++ ) {
+            for(int j = height - i+1; j <= height; j++) {
+                System.out.print("_");
+            }
+            for(int j = 0; j < height - 2*i; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+
+       for(int i=1; i < (height)/2+evenHeight; i++ )
+        {
+            for(int j = 1; j <= height/2 - i; j++)
+                System.out.print("_");
+            for(int j = 0; j < i*2+evenHeight; j++)
+                System.out.print("8");
+            System.out.println();
+        }
+
 
     }
+
+
 }
